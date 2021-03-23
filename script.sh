ccf19ebe964b044bee55771493247d61faf0c1cd
TaenFN <taen.ahammed@fieldnation.com>
2021-03-23 14:29:04 +0600

    Conditional Statement

--- script.sh
+++ script.sh
@@ -1,11 +1,56 @@
 #! /bin/bash
 
-echo $0 $1 $2 $3 ' > echo $0 $1 $2 $3 '
+# integer comparison
 
-args=("$@")
+# -eq - is equal to - if [ "$a" -eq "$b" ]
+# -ne - is not equal to - if [ "$a" -ne "$b" ]
+# -gt - is greater that - if [ "$a" -gt "$b" ]
+# -ge - is greater that or equal to - if ["$a" -ge "$b"]
+# -lt - is less that - if [ "$a" -lt "$b" ]
+# -le - is less that or equal to - if [ "$a" -le "$b" ]
+# < - is less that - (("$a" < "$b"))
+# <= - is less than or equal to - (("$a" <= "$b"))
+# > - is greater than - (("$a" > "$b" ))
+# >= - is greater than or equal to (("$a" >= "$b"))
 
-echo ${args[0]}, ${args[1]}, ${args[2]}
+# string comparision
+# = - is equal to - if [ "$a" = "$b" ]
+# == - is equal to - if [ "$a" == "$b" ]
+# != - is not equal to - if [ "$a" != "$b" ]
+# < - is less than, in ASCII alphabetical order - if [[ "$a" < "$b" ]]
+# > - is greater than, in ASCII alphabetical order - if [[ "$a" > "$b" ]]
+# -z - string is null, that is, has zero lenght
 
-echo $@
+count=10
 
-echo $#
+if [ $count -eq 10 ]
+then
+    echo "condition is true"
+fi
+
+if (( $count > 9 ))
+then
+    echo "condition is true"
+fi
+
+word=a
+
+if [ $word == "abccc" ]
+then
+    echo "condition is true"
+fi
+
+if [[ $word < "a" ]]
+then
+    echo "condition is true"
+fi
+
+if [[ $word == "b" ]]
+then
+    echo "condition b is true"
+elif [[ $word == "a" ]]
+then
+    echo "condition a is true"
+else
+    echo "condition is false"
+fi
