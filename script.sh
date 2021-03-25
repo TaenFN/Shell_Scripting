872eaa92dbd1264da48a37b3784e508ab502732a
TaenFN <taen.ahammed@fieldnation.com>
2021-03-25 06:47:27 +0600

    Floating point math operations

--- script.sh
+++ script.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
 
-num1=20
+num1=20.5
 num2=5
 
-echo $(( num1 + num2 ))
-echo $(( num1 - num2 ))
-echo $(( num1 * num2 ))
-echo $(( num1 / num2 ))
-echo $(( num1 % num2 ))
+echo "20.5+5" | bc
+echo "20.5-5" | bc
+echo "20.5*5" | bc
+echo "scale=2;20.5/5" | bc
+echo "20.5%5" | bc
 
-echo $(expr $num1 + $num2 )
-echo $(expr $num1 - $num2 )
-echo $(expr $num1 \* $num2 )
-echo $(expr $num1 / $num2 )
-echo $(expr $num1 % $num2 )
+echo "$num1+$num2" | bc
+echo "$num1-$num2" | bc
+echo "$num1*$num2" | bc
+echo "scale=2;$num1/$num2" | bc
+echo "$num1%$num2" | bc
+
+num=4
+
+echo "scale=2;sqrt($num)" | bc -l
+echo "scale=2;3^3" | bc -l
